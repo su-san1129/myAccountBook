957561cfe9989eb8ca99b625904d0668573d6120
su-san1129 <deviationvalue1023@outlook.jp>
2021-02-08 18:44:26 +0900

    first commit

---
+++ src/main/java/jp/co/demo/myAccountBook/MyAccountBookApplication.java
@@ -0,0 +1,13 @@
+package jp.co.demo.myAccountBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAccountBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/demo/myAccountBook/MyAccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.demo.myAccountBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
